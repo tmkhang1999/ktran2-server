25c828ae5ee980a8c23e0d1b246e972414770b7d
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2022-10-21 21:19:35 -0400

    Create the config struct

---
+++ modules/structs/config.go
@@ -0,0 +1,15 @@
+package structs
+
+import "time"
+
+type Config struct {
+	Url    string        `yaml:"URL"`
+	Method string        `yaml:"METHOD"`
+	Query  string        `yaml:"QUERY"`
+	Time   time.Duration `yaml:"TIME"`
+
+	TableName string `yaml:"TABLENAME"`
+	Region    string `yaml:"REGION"`
+
+	AccessKey string
+}
