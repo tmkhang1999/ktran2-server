ed8c0be0131d16f533470fde51ee77d5d6f9ecd4
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2022-10-21 21:22:41 -0400

    Update main.go

--- modules/main.go
+++ modules/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"github.com/jamespearly/loggly"
+	"log"
+	"main.go/utils"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("first submit")
+	// Load variables
+	_, err := utils.LoadConfig("./", "config.yaml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Instantiate the user variable including the loggly client
+	user := utils.User{
+		LogglyClient: loggly.New("Weather-App"),
+	}
+
+	// Set up mux router
+	r := mux.NewRouter()
+	r.HandleFunc("/ktran2/status", user.StatusHandler).Methods("GET")
+
+	// Running
+	err = http.ListenAndServe(":4000", r)
+	if err != nil {
+		return
+	}
 }
